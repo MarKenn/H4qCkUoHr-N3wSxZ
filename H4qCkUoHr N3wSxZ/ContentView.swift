26aca417016784202202cd335b5dc8901d6c3979
Mark Kenneth M. Bayona <mkmbayona@gmail.com>
2021-08-08 00:53:27 +0800

    Create project

---
+++ H4qCkUoHr N3wSxZ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4qCkUoHr N3wSxZ
+//
+//  Created by Mark Kenneth Bayona on 8/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4qCkUoHr N3wSxZ/H4qCkUoHr_N3wSxZApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4qCkUoHr_N3wSxZApp.swift
+//  H4qCkUoHr N3wSxZ
+//
+//  Created by Mark Kenneth Bayona on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct H4qCkUoHr_N3wSxZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
